9c5383ffd1ec0dc3f7aea1fa91da65790f68ab1b
bijoy.valsan@cognizant.com <611825@cognizant.com>
2017-04-19 14:48:56 +0530

    'changed'

--- server.js
+++ server.js
@@ -2,31 +2,18 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-// var options = {
-//   key: fs.readFileSync('./fake-keys/privatekey.pem'),
-//   cert: fs.readFileSync('./fake-keys/certificate.pem')
-// };
+
 var serverPort = 3000;
-var https = require('https');
-var http = require('http');
-var server;
-// if (process.env.LOCAL) {
-//   server = https.createServer(options, app);
-// } else {
-  server = http.createServer(app);
-//}
+
+const server = express()
+  .use((req, res) => res.sendFile(__dirname + '/Index.html') )
+  .listen(serverPort, () => console.log('Listening on '+ serverPort));
+
+
 var io = require('socket.io')(server);
 
+
 var roomList = {};
-
-app.get('/', function(req, res){
-  console.log('get /');
-  res.sendFile(__dirname + '/Index.html');
-});
-
-server.listen(serverPort, function(){
-  console.log('server up and running at %s port', serverPort); 
-});
 
 function socketIdsInRoom(name) {
   var socketIds = io.nsps['/'].adapter.rooms[name];
